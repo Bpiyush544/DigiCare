299c69f36ebbb1019e01042902b24d93742f2e1d
Piyush <piyushb544@gmail.com>
2022-10-31 21:40:41 +0530

    models created (patient,doctor)

--- DigiCare/main_app/models.py
+++ DigiCare/main_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class patient(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+    is_patient = models.BooleanField(default=True)
+    is_doctor = models.BooleanField(default=False)
+
+    name = models.CharField(max_length = 50)
+    dob = models.DateField()
+    address = models.CharField(max_length = 100)
+    mobile_no = models.CharField(max_length = 15)
+    gender = models.CharField(max_length = 10)
+
+    
+    @property
+    def age(self):
+        today = date.today()
+        db = self.dob
+        age = today.year - db.year
+        if today.month < db.month or today.month == db.month and today.day < db.day:
+            age -= 1
+        return age 
+
+
+
+class doctor(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    
+    is_patient = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=True)
+
+    name = models.CharField(max_length = 50)
+    dob = models.DateField()
+    address = models.CharField(max_length = 100)
+    mobile_no = models.CharField(max_length = 15)
+    gender = models.CharField(max_length = 10)
+
+    registration_no = models.CharField(max_length = 20)
+    year_of_registration = models.DateField()
+    qualification = models.CharField(max_length = 20)
+    State_Medical_Council = models.CharField(max_length = 30)
+
+    specialization = models.CharField(max_length = 30)
+
+    rating = models.IntegerField(default=0)
+
