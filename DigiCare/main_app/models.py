3583ea14bd7a61367f1119b1e81778523f8a165e
Piyush <piyushb544@gmail.com>
2022-11-01 05:33:07 +0530

    all models setup

--- DigiCare/main_app/models.py
+++ DigiCare/main_app/models.py
@@ -49,3 +49,45 @@ class doctor(models.Model):
 
     rating = models.IntegerField(default=0)
 
+
+class diseaseinfo(models.Model):
+
+    patient = models.ForeignKey(patient , null=True, on_delete=models.SET_NULL)
+
+    diseasename = models.CharField(max_length = 200)
+    no_of_symp = models.IntegerField()
+    symptomsname = models.CharField(max_length=200)
+    confidence = models.DecimalField(max_digits=5, decimal_places=2)
+    consultdoctor = models.CharField(max_length = 200)
+
+
+class consultation(models.Model):
+    
+    patient = models.ForeignKey(patient ,null=True, on_delete=models.SET_NULL)
+    doctor = models.ForeignKey(doctor ,null=True, on_delete=models.SET_NULL)
+    diseaseinfo = models.OneToOneField(diseaseinfo, null=True, on_delete=models.SET_NULL)
+    consultation_date = models.DateField()
+    status = models.CharField(max_length = 20)
+
+
+
+class rating_review(models.Model):
+    
+    patient = models.ForeignKey(patient ,null=True, on_delete=models.SET_NULL)
+    doctor = models.ForeignKey(doctor ,null=True, on_delete=models.SET_NULL)
+    
+    rating = models.IntegerField(default=0)
+    review = models.TextField( blank=True ) 
+
+
+    @property
+    def rating_is(self):
+        new_rating = 0
+        rating_obj = rating_review.objects.filter(doctor=self.doctor)
+        for i in rating_obj:
+            new_rating += i.rating
+       
+        new_rating = new_rating/len(rating_obj)
+        new_rating = int(new_rating)
+        
+        return new_rating
